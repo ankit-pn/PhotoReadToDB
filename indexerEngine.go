7caac36c1957e00c704b5e6c0055cff122d4c9df
ankit-pn <miankitsingh@gmail.com>
2023-11-02 08:21:24 +0530

    added batch processing to db

--- indexerEngine.go
+++ indexerEngine.go
@@ -8,105 +8,110 @@ import (
 	"log"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/writeconcern"
 	"context"
 	"sync"
 )
 
 
 const MaxWorkers = 16
+const batchSize = 100
 
-func worker(filesChan <-chan string, wg *sync.WaitGroup, collection *mongo.Collection) {
-	defer wg.Done()
+func worker(filesChan <-chan string, wg *sync.WaitGroup, collection *mongo.Collection, ctx context.Context) {
+    defer wg.Done()
+    batch := make([]interface{}, 0, batchSize)
 
-	for path := range filesChan {
-		fileData, err := extractText(path)
-		if err != nil {
-			fmt.Printf("error extracting text from %q: %v\n", path, err)
-			continue
-		}
+    for path := range filesChan {
+        fileData, err := extractText(path) // extractText needs to be defined
+        if err != nil {
+            fmt.Printf("error extracting text from %q: %v\n", path, err)
+            continue
+        }
 
-		fileName := filepath.Base(path)
-		file := File{
-			FileName: fileName,
-			FilePath: path,
-			FileData: fileData,
-		}
-		// Log the entire file struct to the terminal
-		// fmt.Printf("Processed File: %+v\n", file)
+        fileName := filepath.Base(path)
+        file := File{
+            FileName: fileName,
+            FilePath: path,
+            FileData: fileData,
+        }
 
-		// Insert the File data into MongoDB
-		_, err = collection.InsertOne(context.TODO(), file)
-		if err != nil {
-			fmt.Printf("error inserting data into MongoDB: %v\n", err)
-			continue
-		}
-		// Log successful saving to the terminal
-		// fmt.Println("Successfully saved to MongoDB:", file.FileName)
-	}
+        batch = append(batch, file)
+
+        if len(batch) >= batchSize {
+            if _, err = collection.InsertMany(ctx, batch); err != nil {
+                fmt.Printf("error inserting batch into MongoDB: %v\n", err)
+            }
+            // Clear the batch slice for next batch
+            batch = batch[:0]
+        }
+    }
+
+    // Insert any remaining files
+    if len(batch) > 0 {
+        if _, err := collection.InsertMany(ctx, batch); err != nil {
+            fmt.Printf("error inserting final batch into MongoDB: %v\n", err)
+        }
+    }
 }
 
-
-
-
-
-
 func indexerEngine(rootPath string) {
-	mongoURI := os.Getenv("MONGO_URI")
-	if mongoURI == "" {
-		log.Fatal("MONGO_URI not set in environment")
-	}
+    mongoURI := os.Getenv("MONGO_URI")
+    if mongoURI == "" {
+        log.Fatal("MONGO_URI not set in environment")
+    }
 
-	// Set client options
-	clientOptions := options.Client().ApplyURI(mongoURI)
+    // Set client options with write concern for better performance
+    clientOptions := options.Client().ApplyURI(mongoURI).SetWriteConcern(writeconcern.New(writeconcern.W(1)))
 
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Connect to MongoDB
+    client, err := mongo.Connect(context.TODO(), clientOptions)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Check the connection
+    err = client.Ping(context.TODO(), nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer func() {
+        if err = client.Disconnect(context.TODO()); err != nil {
+            log.Fatal(err)
+        }
+    }()
 
-	fmt.Println("Connected to MongoDB!")
+    fmt.Println("Connected to MongoDB!")
 
-	// Get a handle for your collection
-	collection := client.Database("tesseract").Collection("images")
+    collection := client.Database("tesseract").Collection("images")
 
-	filesChan := make(chan string, MaxWorkers)
-	var wg sync.WaitGroup
+    filesChan := make(chan string, MaxWorkers)
+    var wg sync.WaitGroup
+    ctx, cancel := context.WithCancel(context.Background())
 
-	// Start workers
-	for i := 0; i < MaxWorkers; i++ {
-		wg.Add(1)
-		go worker(filesChan, &wg, collection)
-	}
+    // Start workers
+    for i := 0; i < MaxWorkers; i++ {
+        wg.Add(1)
+        go worker(filesChan, &wg, collection, ctx)
+    }
 
-	// Walking the directory structure and sending files to the channel
-	walkErr := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			fmt.Printf("error accessing path %q: %v\n", path, err)
-			return err
-		}
+    // Walking the directory structure and sending files to the channel
+    walkErr := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            fmt.Printf("error accessing path %q: %v\n", path, err)
+            return err
+        }
 
-		if !info.IsDir() && strings.HasSuffix(strings.ToLower(info.Name()), ".jpeg") {
-			filesChan <- path
-		}
-		return nil
-	})
+        if !info.IsDir() && strings.HasSuffix(strings.ToLower(info.Name()), ".jpeg") {
+            filesChan <- path
+        }
+        return nil
+    })
 
-	// Handle potential walk error
-	if walkErr != nil {
-		fmt.Printf("error walking the path %v: %v\n", rootPath, walkErr)
-		// You might want to signal your workers to stop processing at this point.
-	}
+    if walkErr != nil {
+        fmt.Printf("error walking the path %v: %v\n", rootPath, walkErr)
+    }
 
-	// Safely close the filesChan when all the files have been sent to it
-	defer close(filesChan)
-
-	// Wait for all workers to finish
-	wg.Wait()
+    close(filesChan)
+    wg.Wait()
+    cancel() // Cancel the context to free resources
 }
